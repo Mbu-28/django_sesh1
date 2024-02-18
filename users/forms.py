2011519133102e1b5339d10279a5157e950d1467
Mbu-28 <mbulzamiya@gmail.com>
2024-02-18 15:44:16 +0200

    Profile update

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 # creation of from that inherits from UserCreationForm, added a fied for email 
 class UserRegisterForm(UserCreationForm):
@@ -9,3 +10,18 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User 
         fields = ['username', 'email', 'password1', 'password2']
+
+
+#ModelForm. Updates user model
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User 
+        fields = ['username' , 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
